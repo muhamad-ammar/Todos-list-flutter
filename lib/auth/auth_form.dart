56c7a7479cedd1edd012468c55be33f202d7097e
muhammad-amar <ammar.bscs18seecs.edu.pk>
2023-01-12 10:31:48 +0500

    Authentication is in progress

---
+++ lib/auth/auth_form.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+    );
+  }
+}
