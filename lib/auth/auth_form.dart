6c0ac1d7840755b413ba03442df4e6770d355fba
muhammad-amar <ammar.bscs18seecs.edu.pk>
2023-01-12 11:36:27 +0500

    Login Screen design Complete

--- lib/auth/auth_form.dart
+++ lib/auth/auth_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
@@ -8,11 +9,119 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formkey = GlobalKey<FormState>();
+  var _email = '';
+  var _password = '';
+  var _username = '';
+  bool isloginPage = false;
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(10),
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
+      child: ListView(
+        padding: EdgeInsets.only(
+          left: 10,
+          right: 10,
+          top: 20,
+          bottom: 20,
+        ),
+        children: [
+          Container(
+            child: Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    if (!isloginPage)
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        key: ValueKey('username'),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Incorrect Username';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _username = value!;
+                        },
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: new BorderRadius.circular(8),
+                                borderSide: new BorderSide()),
+                            labelText: 'Enter Username',
+                            labelStyle: GoogleFonts.roboto()),
+                      ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      key: ValueKey('email'),
+                      validator: (value) {
+                        if (value!.isEmpty || !value.contains('@')) {
+                          return 'Incorrect Email';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _email = value!;
+                      },
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: new BorderRadius.circular(8),
+                              borderSide: new BorderSide()),
+                          labelText: 'Enter Email',
+                          labelStyle: GoogleFonts.roboto()),
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      key: ValueKey('password'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Incorrect password';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _password = value!;
+                      },
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: new BorderRadius.circular(8),
+                              borderSide: new BorderSide()),
+                          labelText: 'Enter Password',
+                          labelStyle: GoogleFonts.roboto()),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                        padding: EdgeInsets.all(5),
+                        height: 70,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.green),
+                        child: TextButton(
+                          child: isloginPage
+                              ? Text(
+                                  'Login',
+                                  style: GoogleFonts.roboto(
+                                      fontSize: 20, color: Colors.white),
+                                )
+                              : Text('Sign Up',
+                                  style: GoogleFonts.roboto(
+                                      fontSize: 20, color: Colors.white)),
+                          onPressed: () {},
+                        )),
+                  ],
+                )),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/auth/auth_screen.dart';
 import 'package:todo/screens/home.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Home(),
+        home: AuthScreen(),
         debugShowCheckedModeBanner: false,
         theme:
             ThemeData(brightness: Brightness.dark, primaryColor: Colors.red));
