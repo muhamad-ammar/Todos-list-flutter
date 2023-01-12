983695ff27f1b0daf7fdf19fb761507ec7869a80
muhammad-amar <ammar.bscs18seecs.edu.pk>
2023-01-12 10:07:43 +0500

    Building from scratch for Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,19 @@
 import 'package:flutter/material.dart';
-import 'todo_list.dart';
+import 'package:todo/screens/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-      ),
-      home: const Todos(),
-    );
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+        theme:
+            ThemeData(brightness: Brightness.dark, primaryColor: Colors.red));
   }
 }
